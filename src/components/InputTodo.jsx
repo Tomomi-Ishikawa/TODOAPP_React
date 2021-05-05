c93b0e2e45c4f314bbdedccdd482c901bbf72f52
Tomomi-Ishikawa <68460000+Tomomi-Ishikawa@users.noreply.github.com>
2021-05-05 20:28:36 +0900

    css in js

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 
+// styles
+// CSS in JS
+const style = {
+  backgroundColor: "#c1fffc",
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px"
+};
+
 // コンポーネント化したら, propsで　関数を引数として渡す
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-    <div class="input-area">
+    <div style={style}>
       <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
       <button onClick={onClick}> 追加 </button>
     </div>
